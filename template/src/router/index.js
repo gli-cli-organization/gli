d4af392e57688727bf67f77db538616753fae44f
lichuanzhen <1134410688@qq.com>
2018-12-21 16:04:03 +0800

    f放开

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import Router from 'vue-router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-const IndexPage = (resolve)=>{require(['../page/IndexPage.vue'], resolve)};
+const IndexPage = (resolve) =>{require(['../page/IndexPage.vue'], resolve)};
 
 
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
